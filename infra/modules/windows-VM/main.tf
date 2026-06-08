86195814f1eece950e81aa24d05d34ee7a5b085a
akash-patil-16 <akashpatil302.ap@gmail.com>
2026-06-08 14:38:51 +0530

    Fix size issue in location

--- infra/modules/windows-VM/main.tf
+++ infra/modules/windows-VM/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_windows_virtual_machine" "vm_main" {
   name                = var.name
   resource_group_name = var.resource_group_name
   location            = var.location
-  size                = "Standard_F2"
+  size                = "Standard_B1s"
   admin_username      = var.admin_username
   admin_password      = var.admin_password
   network_interface_ids =  var.network_interface_ids
