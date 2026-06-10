9cba476d232bb7a5e80f2e92efd5167f5330e96e
akash-patil-16 <akashpatil302.ap@gmail.com>
2026-06-10 18:43:49 +0530

    Commit changes for qa env

---
+++ infra/envs/qa/main.tf
@@ -0,0 +1,88 @@
+module "resource-group" {
+  source   = "../../modules/resource-group"
+  for_each = var.resource_group
+  name     = each.value.name
+  location = each.value.location
+}
+
+
+module "storage-account" {
+  source                   = "../../modules/storage-account"
+  for_each                 = var.storage_account
+  name                     = each.value.name
+  resource_group_name      = module.resource-group[each.value.resource_group_key].name
+  location                 = module.resource-group[each.value.resource_group_key].location
+  account_tier             = each.value.account_tier
+  account_replication_type = each.value.account_replication_type
+}
+
+module "Vnet" {
+
+  source              = "../../modules/Vnet"
+  for_each            = var.Vnet
+  name                = each.value.name
+  resource_group_name = module.resource-group[each.value.resource_group_key].name
+  location            = module.resource-group[each.value.resource_group_key].location
+  address_space       = each.value.address_space
+}
+
+module "subnet" {
+  source               = "../../modules/subnet"
+  for_each             = var.subnet
+  name                 = each.value.name
+  resource_group_name  = module.resource-group[each.value.resource_group_key].name
+  virtual_network_name = module.Vnet[each.value.vnet_key].name
+  address_prefixes     = each.value.address_prefixes
+}
+
+module "pip-VM" {
+  source              = "../../modules/pip-VM"
+  for_each            = var.pip-VM
+  pip_name            = each.value.pip_name
+  resource_group_name = module.resource-group[each.value.resource_group_key].name
+  location            = module.resource-group[each.value.resource_group_key].location
+}
+
+module "nsg-VM" {
+  source              = "../../modules/nsg-VM"
+  for_each            = var.nsg_VM
+  nsg_name            = each.value.nsg_name
+  location            = module.resource-group[each.value.resource_group_key].location
+  resource_group_name = module.resource-group[each.value.resource_group_key].name
+}
+
+module "nic-VM" {
+  source               = "../../modules/nic-VM"
+  for_each             = var.nic_VM
+  name                 = each.value.name
+  location             = module.resource-group[each.value.resource_group_key].location
+  resource_group_name  = module.resource-group[each.value.resource_group_key].name
+  subnet_id            = module.subnet[each.value.subnet_key].id
+  ip_name              = each.value.ip_name
+  public_ip_address_id = module.pip-VM[each.value.pip_key].id
+  nsg_id               = module.nsg-VM[each.value.nsg_key].id
+}
+
+# module "windows-VM" {
+#   source                = "../../modules/windows-VM"
+#   for_each              = var.windows-VM
+#   name                  = each.value.name
+#   location              = module.resource-group[each.value.resource_group_key].location
+#   resource_group_name   = module.resource-group[each.value.resource_group_key].name
+#   network_interface_ids = [module.nic-VM[each.value.nic_VM_key].id]
+#   admin_username        = var.admin_username
+#   admin_password        = var.admin_password
+# }
+
+module "linux-VM" {
+  source                = "../../modules/linux-VM"
+  for_each              = var.linux-VM
+  name                  = each.value.name
+  location              = module.resource-group[each.value.resource_group_key].location
+  resource_group_name   = module.resource-group[each.value.resource_group_key].name
+  network_interface_ids = [module.nic-VM[each.value.nic_VM_key].id]
+  admin_username        = var.admin_username
+  admin_password        = var.admin_password
+
+}
+
