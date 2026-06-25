4725ac53d2b8aacbf9e76ea5ccea37c65a0a684c
akash-patil-16 <akashpatil302.ap@gmail.com>
2026-06-25 14:22:52 +0530

    revert change

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -19,7 +19,6 @@ module "storage-account" {
 module "Vnet" {
 
   source              = "../../modules/Vnet"
-  depends_on          = [module.vnet]
   for_each            = var.Vnet
   name                = each.value.name
   resource_group_name = module.resource-group[each.value.resource_group_key].name
@@ -29,6 +28,7 @@ module "Vnet" {
 
 module "subnet" {
   source               = "../../modules/subnet"
+  depends_on           = [module.Vnet]
   for_each             = var.subnet
   name                 = each.value.name
   resource_group_name  = module.resource-group[each.value.resource_group_key].name
