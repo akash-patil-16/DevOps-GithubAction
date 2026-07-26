078ba40b9d9af6ce78c0e23ae918bc714973c1f4
akash-patil-16 <akashpatil302.ap@gmail.com>
2026-07-26 13:01:56 +0530

    Add keyvault for VM username and password

--- infra/modules/linux-VM/main.tf
+++ infra/modules/linux-VM/main.tf
@@ -1,10 +1,26 @@
+data "azurerm_key_vault" "key_vault" {
+  name                = "keyvalutapatil"
+  resource_group_name = "rg-apatil"
+}
+
+data "azurerm_key_vault_secret" "admin_password" {
+  name         = "vmpassword"
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
+data "azurerm_key_vault_secret" "admin_username" {
+  name         = "vmuser"
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
+
 resource "azurerm_linux_virtual_machine" "linux_main" {
   name                = var.name
   resource_group_name = var.resource_group_name
   location            = var.location
   size                = "Standard_DC1s_v3"
-  admin_username      = var.admin_username
-  admin_password      = var.admin_password
+  admin_username      = data.azurerm_key_vault_secret.admin_username.value
+  admin_password      = data.azurerm_key_vault_secret.admin_password.value
   network_interface_ids = var.network_interface_ids
 
   disable_password_authentication = false
