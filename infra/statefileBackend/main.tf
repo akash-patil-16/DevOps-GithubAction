efbb1ae19579a7da64776cc87218ebc3a88caf60
akash-patil-16 <akashpatil302.ap@gmail.com>
2026-08-06 15:16:41 +0530

    Add key vault configuration and secrets to Terraform setup

--- infra/statefileBackend/main.tf
+++ infra/statefileBackend/main.tf
@@ -9,6 +9,10 @@ terraform {
 
 provider "azurerm" {
   features {}
+  key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
 }
 
 resource "azurerm_resource_group" "rg_statefile_backend" {
@@ -28,4 +32,28 @@ resource "azurerm_storage_container" "container_statefile_backend" {
   name                  = "devtfstate"
   storage_account_id    = azurerm_storage_account.statefile_backend.id
   container_access_type = "private"
+}
+
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "keyvault_statefile_backend" {
+  name                        = "keyvalutapatil"
+  resource_group_name         = azurerm_resource_group.rg_statefile_backend.name
+  location                    = azurerm_resource_group.rg_statefile_backend.location
+  sku_name                    = "standard"
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  purge_protection_enabled    = false
+}
+
+resource "azurerm_key_vault_secret" "secret_statefile_backend" {
+  name         = "vmpassword"
+  value        = "Akash@12345@"
+  key_vault_id = azurerm_key_vault.keyvault_statefile_backend.id
+}
+
+resource "azurerm_key_vault_secret" "secret_statefile_backend" {
+  name         = "vmuser"
+  value        = "adminakash"
+  key_vault_id = azurerm_key_vault.keyvault_statefile_backend.id
 }
